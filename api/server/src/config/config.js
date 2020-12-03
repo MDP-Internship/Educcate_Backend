8abe8ce766bfe40ed0670e0982418b41d4285340
Abdullah Oğuz <oguzabdullah39@gmail.com>
2020-12-03 15:41:06 +0300

    teplate added

--- api/server/src/config/config.js
+++ api/server/src/config/config.js
@@ -8,7 +8,7 @@ module.exports = {
 
   development: {
     database: "books",
-    username: "steven",
+    username: "postgres",
     password: "admin",
     host: "127.0.0.1",
     dialect: "postgres",
