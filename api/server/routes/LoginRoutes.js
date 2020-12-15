e2dd4d0ad10226c6a1e085a17e83b1f6c8e9a35d
Abdullah Oğuz <oguzabdullah39@gmail.com>
2020-12-15 09:03:16 +0300

    loginservices ve routes eklendi

---
+++ api/server/routes/LoginRoutes.js
@@ -0,0 +1,9 @@
+import { Router } from "express"
+import LoginController from "../controllers/LoginController"
+
+const router = Router()
+
+router.post("/register", LoginController.register)
+router.post("/login", LoginController.login)
+
+export default router
---
+++ api/server/services/LoginService.js
@@ -0,0 +1,20 @@
+import db from "../src/models"
+
+class LoginService {
+  static async register(name, surname, email, password, roleId) {
+    try {
+      return await db.User.create({ name, surname, password, email, roleId })
+    } catch (err) {
+      throw err
+    }
+  }
+  static async login(email) {
+    try {
+      const findUser = await db.User.findOne({ where: { email } })
+      return findUser
+    } catch (err) {
+      throw err
+    }
+  }
+}
+export default LoginService
