29418da37f17e2adbfe7cddf3bef02e99c8067e0
Abdullah Oğuz <oguzabdullah39@gmail.com>
2020-12-19 12:37:20 +0300

    edit routes

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 import config from "dotenv"
 import express from "express"
 import bodyParser from "body-parser"
-import bookRoutes from "./server/routes/BookRoutes"
+import cors from "cors"
 import loginRoutes from "./server/routes/LoginRoutes"
 
 config.config()
@@ -12,8 +12,8 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
 const port = process.env.PORT || 8000
+app.use(cors())
 
-app.use("/api/v1/books", bookRoutes)
 app.use("/user", loginRoutes)
 
 // when a random route is inputed
