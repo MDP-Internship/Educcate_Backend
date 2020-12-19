d4905e7606356523c9902f4fac455bc4aecd6d2b
Abdullah Oğuz <oguzabdullah39@gmail.com>
2020-12-19 12:38:42 +0300

    middleware added

---
+++ api/server/middleware/checkauth.js
@@ -0,0 +1,35 @@
+import jwt from "jsonwebtoken"
+import express from "express"
+import bcrypt from "bcrypt"
+import {encrypText} from "../src/config/settings"
+import Helpers from "../utils/Util"
+import Util from "../utils/Util"
+import LoginRoutes from "../routes/LoginRoutes"
+import router from "../routes/LoginRoutes"
+
+const app  = express(),
+const util  = new Util()
+
+
+app.use(async(req,res,next)=>{
+
+    const token = req.headers.authorization
+    if (!token) {
+        util.send(401,"Token cannot empty");
+        return util.send(res);
+    }
+    jwt.verify(token, encrypText, (err,decoded)=>{
+        if (err) {
+            util.setError(401, "Your Access is denied")
+            return util.send(res)  
+        }
+        util.setError(200, "Your access is succesfull");
+        req.decoded = decoded;
+        next(); 
+    });
+});
+
+
+
+
+
