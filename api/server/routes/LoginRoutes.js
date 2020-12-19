3def4ec960aeaa8dd5bc3c94c3260d0eb78069b3
Abdullah Oğuz <oguzabdullah39@gmail.com>
2020-12-19 12:39:03 +0300

    login routes added

--- api/server/routes/LoginRoutes.js
+++ api/server/routes/LoginRoutes.js
@@ -5,5 +5,6 @@ const router = Router()
 
 router.post("/register", LoginController.register)
 router.post("/login", LoginController.login)
+router.post("/change-password", LoginController.changePassword)
 
 export default router
