8f0c122d82a3e78d7c1ed96546061ebbc1438e36
Abdullah Oğuz <oguzabdullah39@gmail.com>
2020-12-27 15:01:48 +0300

    edit heroku

--- api/index.js
+++ api/index.js
@@ -25,7 +25,7 @@ app.get("*", (req, res) =>
     message: "Welcome to this API.",
   })
 )
-const porta = process.env.PORT || 8080
-app.listen(porta, () => console.log("Example app listening on port 8080!"))
+
+app.listen(port, () => console.log(`server is running on port ${port}`))
 
 export default app
--- api/server/src/config/config.js
+++ api/server/src/config/config.js
@@ -2,33 +2,14 @@ require("dotenv").config()
 
 module.exports = {
   // If using onine database
-  development: {
-    url:
-      "postgres://mhucwllkkkcjlr:765fe3938d074deab0f1b32c535fa7a0729690158b3ec107d52048531b56a141@ec2-3-89-230-115.compute-1.amazonaws.com:5432/d83vl5jf0j7mgb",
-    dialect: "postgres",
-    protocol: "postgres",
-    dialectOptions: {
-      ssl: {
-        rejectUnauthorized: false,
-      },
-    },
-  },
 
-  /*     development: {
-    database: "edducate",
-    username: "postgres",
-    password: "admin",
-    host: "127.0.0.1",
-    dialect: "postgres", //hangi databese ile çalıştığın (örnek mysql de olabilirdi)
-  }, */
-  /* 
   development: {
     database: "edducate",
     username: "postgres",
     password: "admin",
     host: "127.0.0.1",
     dialect: "postgres", //hangi databese ile çalıştığın (örnek mysql de olabilirdi)
-  }, */
+  },
 
   /*   test: {
     database: "book_test",
@@ -39,7 +20,6 @@ module.exports = {
   }, */
 
   production: {
-    use_env_variable: "DATABASE_URL",
     database: "d83vl5jf0j7mgb",
     username: "mhucwllkkkcjlr",
     password:
