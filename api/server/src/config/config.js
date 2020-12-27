14797b78cc0e7bc3369f6b03d19125af48c823ba
Abdullah Oğuz <oguzabdullah39@gmail.com>
2020-12-27 15:07:36 +0300

    edit config

--- api/server/src/config/config.js
+++ api/server/src/config/config.js
@@ -22,8 +22,7 @@ module.exports = {
   production: {
     database: "d83vl5jf0j7mgb",
     username: "mhucwllkkkcjlr",
-    password:
-      "765fe3938d074deab0f1b32c535fa7a0729690158b3ec107d52048531b56a141",
+    password:"765fe3938d074deab0f1b32c535fa7a0729690158b3ec107d52048531b56a141",
     host: "ec2-35-168-77-215.compute-1.amazonaws.com",
     dialect: "postgres",
   },
