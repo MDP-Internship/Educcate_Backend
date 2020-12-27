e32c2ab3c10f5757cc7b48bb68005c3891613e94
Abdullah Oğuz <oguzabdullah39@gmail.com>
2020-12-27 15:19:23 +0300

    edit env

--- api/server/src/config/config.js
+++ api/server/src/config/config.js
@@ -6,7 +6,8 @@ module.exports = {
   development: {
     database: "edducate",
     username: "postgres",
-    password: "admin",
+    password: 
+    "admin",
     host: "127.0.0.1",
     dialect: "postgres", //hangi databese ile çalıştığın (örnek mysql de olabilirdi)
   },
@@ -20,10 +21,11 @@ module.exports = {
   }, */
 
   production: {
-    database: "d83vl5jf0j7mgb",
-    username: "mhucwllkkkcjlr",
-    password:"765fe3938d074deab0f1b32c535fa7a0729690158b3ec107d52048531b56a141",
-    host: "ec2-35-168-77-215.compute-1.amazonaws.com",
-    dialect: "postgres",
-  },
+    database: process.env.DB_NAME,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    host: process.env.DB_HOST,
+    dialect: 'postgres'
+    }
+  
 }
