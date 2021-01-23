fe7d603420aedb1260962128f396919878da1455
Abdullah Oğuz <oguzabdullah39@gmail.com>
2021-01-24 01:07:22 +0300

    private files user route finish

--- api/server/private/route/UserRoute.js
+++ api/server/private/route/UserRoute.js
@@ -6,9 +6,12 @@ const router = Router()
 
 router.get("/getAll", PrivateController.userGetAll)
 router.get("/rateGet", PrivateController.getRates)
+router.get("/getRateInfo/:id", PrivateController.getRate)
 router.get("/getAllRate", PrivateController.getAllRate)
-router.get("/userRate/:id", PrivateController.getUserRate)
-router.get("/buyRate", PrivateController.buyRate)
+router.get("/rateUser/:id", PrivateController.getRateUser)
+router.get("/buyRate/:id", PrivateController.buyRate)
+router.get("/userRate", PrivateController.userRate)
+
 
 
 
