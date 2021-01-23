06cef906cfe931bbdddfab390f433627b718d482
Abdullah Oğuz <oguzabdullah39@gmail.com>
2021-01-24 01:06:26 +0300

    admin route added

--- api/server/admin/routes/AdminRoute.js
+++ api/server/admin/routes/AdminRoute.js
@@ -6,5 +6,6 @@ const router = Router()
 router.put("/delete/:id", AdminController.deleteUser)
 router.put("/update/:id", AdminController.updateUserInfo)
 router.post("/addRate", AdminController.addRate)
+router.put("/updateRate/:id",AdminController.updateRate)
 
 export default router
