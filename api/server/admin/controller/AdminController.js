089dd7b0fc24585f3f75e07a71104345b8b6190f
Abdullah Oğuz <oguzabdullah39@gmail.com>
2021-01-24 01:06:17 +0300

    admin controller finish

--- api/server/admin/controller/AdminController.js
+++ api/server/admin/controller/AdminController.js
@@ -2,20 +2,30 @@ import jwt from "jsonwebtoken"
 import bcrypt from "bcrypt"
 import { encryptText } from "../../src/config/settings"
 import AdminService from "../services/AdminService"
+import db from "../../src/models"
 
 class AdminController {
   static async updateUserInfo(req, res) {
     const { id } = req.params
-    const { name, surname, email, roleId, credit, basket } = req.body
+    const {
+      name,
+      surname,
+      email,
+      currency_level,
+      basket,
+      credit,
+      roleId,
+    } = req.body
 
     const updateUser = await AdminService.updateUser(
       id,
       name,
       surname,
       email,
-      roleId,
+      currency_level,
+      basket,
       credit,
-      basket
+      roleId
     )
     console.log(updateUser)
     if (!updateUser) {
@@ -58,7 +68,30 @@ class AdminController {
     })
   }
 
-  
+  static async updateRate(req, res) {
+    const body = req.body
+    const { id } = req.params
+    const data = {
+      name: body.name,
+      rate_hour: body.rate_hour,
+      teacher_name: body.teacher_name,
+      limit: body.limit,
+      price: body.price,
+    }
+    const result = await AdminService.updateRate(data, id)
+    if (!result) {
+      res.json({
+        type: false,
+        message: "Kur güncellenirken hata oluştu",
+      })
+    }
+     const updatedRate = await db.Rate.findOne({where:{id}})
+    res.json({
+      type: true,
+      message: "Kur Güncellendi",
+      data:updatedRate
+    })
+  }
 }
 
 export default AdminController
