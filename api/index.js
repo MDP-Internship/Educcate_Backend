bb9eb0f7d06a6c3f4c883adf072717205142da9c
Abdullah Oğuz <oguzabdullah39@gmail.com>
2021-01-24 17:23:15 +0300

    edit swagger

--- api/index.js
+++ api/index.js
@@ -6,6 +6,9 @@ import userRoutes from "./server/account/routes/UserRoutes"
 import adminRoutes from "./server/admin/index"
 import privateRoutes from "./server/private/index"
 import tokenControl from "./server/middleware/checkauth"
+import swaggerUi from 'swagger-ui-express'
+import swaggerDocument from "../swagger.json"
+
 
 config.config()
 
@@ -15,11 +18,13 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
 const port = process.env.PORT || 8000
+
 app.use(cors())
 app.use("/user", userRoutes)
 //app.use(tokenControl)
 app.use("/admin", adminRoutes)
 app.use("/private", privateRoutes)
+app.use("/api-docs" ,swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // when a random route is inputed
 app.get("*", (req, res) =>
