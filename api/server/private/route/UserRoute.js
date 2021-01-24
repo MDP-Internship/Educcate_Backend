b7ca089b0c22042bcb90f756b4db81d28d1254cf
Abdullah Oğuz <oguzabdullah39@gmail.com>
2021-01-24 11:19:50 +0300

    add getUser route

--- api/server/private/route/UserRoute.js
+++ api/server/private/route/UserRoute.js
@@ -6,6 +6,7 @@ const router = Router()
 
 router.get("/getAll", PrivateController.userGetAll)
 router.get("/rateGet", PrivateController.getRates)
+router.get("/getUser", PrivateController.getUser)
 router.get("/getRateInfo/:id", PrivateController.getRate)
 router.get("/getAllRate", PrivateController.getAllRate)
 router.get("/rateUser/:id", PrivateController.getRateUser)
